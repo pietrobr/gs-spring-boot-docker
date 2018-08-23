b386a8ef80184c2509a9e0b1cebf0c58750c7db5
pietrobr <pbrambati@hotmail.com>
2018-08-23 19:28:57 +0200

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hello Docker World from Azure and Jankins";
     }
 
     public static void main(String[] args) {
